5ae96314fc9d7eeedb6d177f9274142e78a41bfd
Martin Keereman <mkeereman@tableau.com>
2019-02-26 18:43:20 -0800

    Refactor EntityContentOptionForm for destination plugins

---
+++ src/Plugin/migrate/destination/Form/DestinationPluginFormBase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\feeds_migrate\Plugin\migrate\destination\Form;
+
+use Drupal\feeds_migrate\Plugin\ExternalPluginFormBase;
+
+/**
+ * The base configuration form for migrate destination plugins.
+ */
+abstract class DestinationPluginFormBase extends ExternalPluginFormBase {
+
+
+}
