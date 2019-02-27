2248bcbab6c29c5baa59ab98d513d1b122e9f88d
Martin Keereman <mkeereman@tableau.com>
2019-02-26 18:43:42 -0800

    Save values from file plugin to entity

--- src/Plugin/migrate_plus/data_fetcher/Form/FileForm.php
+++ src/Plugin/migrate_plus/data_fetcher/Form/FileForm.php
@@ -2,10 +2,8 @@
 
 namespace Drupal\feeds_migrate\Plugin\migrate_plus\data_fetcher\Form;
 
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\feeds_migrate\FeedsMigrateImporterInterface;
-use Drupal\migrate\Plugin\Migration;
-use Drupal\file\Entity\File as FileEntity;
 
 /**
  * The configuration form for the file migrate data fetcher plugin.
@@ -16,10 +14,14 @@ class FileForm extends DataFetcherPluginFormBase {
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    /** @var \Drupal\migrate_plus\Entity\MigrationInterface $entity */
+    $entity = $this->entity;
+    $source = $entity->get('source');
+
     $form['directory'] = [
       '#type' => 'textfield',
       '#title' => $this->t('File Upload Directory'),
-      '#default_value' => 'public://migrate',
+      '#default_value' => $source['data_fetcher']['directory'] ?: 'public://migrate',
     ];
 
     return $form;
@@ -28,47 +30,8 @@ class FileForm extends DataFetcherPluginFormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    parent::validateForm($form, $form_state);
-    $fids = $form_state->getValue([
-      'dataFetcherSettings',
-      $this->getPluginId(),
-      'file',
-    ]);
-
-    if (empty($fids)) {
-      $form_state->setError($form['dataFetcherSettings'][$this->getPluginId()]['file'], $this->t('File is required'));
-      return;
-    }
-
-    if ($file = FileEntity::load(reset($fids))) {
-      $file->setPermanent();
-      $file->save();
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function alterMigration(FeedsMigrateImporterInterface $importer, Migration $migration) {
-    if (!empty($importer->getFetcherSettings($this->getPluginId()))) {
-      $fids = $importer->getFetcherSettings($this->getPluginId());
-
-
-      if ($file = FileEntity::load(reset($fids['file']))) {
-
-        $source_config = $migration->getSourceConfiguration();
-        $source_config['urls'] = file_create_url($file->getFileUri());
-        $migration->set('source', $source_config);
-      }
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getParserData(array $form, FormStateInterface $form_state) {
-    return $form_state->getValue([$this->getPluginId(), 'url']);
+  public function copyFormValuesToEntity(EntityInterface $entity, array $form, FormStateInterface $form_state) {
+    $entity->source['data_fetcher']['directory'] = $form_state->getValue('directory');
   }
 
 }
