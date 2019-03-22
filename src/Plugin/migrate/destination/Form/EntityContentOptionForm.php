4b3b27095d1327f0c3988ac9e6ca7fa1a3b9e71a
Martin Keereman <mkeereman@tableau.com>
2019-03-21 17:21:31 -0700

    Don't set parents because subformstate expects tree

--- src/Plugin/migrate/destination/Form/EntityContentOptionForm.php
+++ src/Plugin/migrate/destination/Form/EntityContentOptionForm.php
@@ -64,7 +64,6 @@ class EntityContentOptionForm extends DestinationFormPluginBase {
         '#title' => $entity_type->getBundleLabel(),
         '#required' => TRUE,
         '#default_value' => $this->getSetting('default_bundle'),
-        '#parents' => ['migration', 'destination', 'default_bundle'],
       ];
     }
 
@@ -77,7 +76,7 @@ class EntityContentOptionForm extends DestinationFormPluginBase {
   public function copyFormValuesToEntity(EntityInterface $entity, array $form, FormStateInterface $form_state) {
     $entity_type = $this->getEntityType();
     if ($entity_type && $bundle_key = $entity_type->getKey('bundle')) {
-      $entity->destination['default_bundle'] = $form_state->getCompleteFormState()->getValue('migration')['destination']['default_bundle'];
+      $entity->destination['default_bundle'] = $form_state->getValue('default_bundle');
     }
     else {
       unset($entity->destination['default_bundle']);
