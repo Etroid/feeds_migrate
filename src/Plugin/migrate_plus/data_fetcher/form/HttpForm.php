6881177a21fac4fe6abefa68cfe988fd768075ca
Jonathan Franks <jonathan.franks@breaktech.com>
2019-04-09 15:16:23 -0700

    Added data fetcher and data parser forms to collect the correct migration parameters

---
+++ src/Plugin/migrate_plus/data_fetcher/form/HttpForm.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\feeds_migrate\Plugin\migrate_plus\data_fetcher\form;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * The configuration form for the file migrate data fetcher plugin.
+ *
+ * @MigrateForm(
+ *   id = "http",
+ *   title = @Translation("Http Fetcher Plugin Form"),
+ *   form = "configuration",
+ *   parent_id = "http",
+ *   parent_type = "data_fetcher"
+ * )
+ */
+class HttpForm extends DataFetcherFormPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    /** @var \Drupal\migrate_plus\Entity\MigrationInterface $entity */
+    $entity = $this->entity;
+    $source = $entity->get('source');
+
+    $form['urls'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('File Location (single location only)'),
+      '#default_value' => $source['data_fetcher']['urls'] ?: 'public://migrate/data.xml',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function copyFormValuesToEntity(EntityInterface $entity, array $form, FormStateInterface $form_state) {
+    $entity->source['data_fetcher']['urls'] = $form_state->getValue('urls');
+  }
+
+}
