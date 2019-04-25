3f5ce59893746a814e6a94d631a8239f5a7a1202
Martin Keereman <mkeereman@tableau.com>
2019-04-25 07:12:23 -0700

    Stub functional test for importer

---
+++ tests/src/FunctionalJavascript/Form/ImporterFormTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\Tests\feeds_migrate\FunctionalJavascript\Form;
+
+use Drupal\Tests\feeds_migrate\FunctionalJavascript\FeedsMigrateJavascriptTestBase;
+
+/**
+ * Tests adding and editing feeds migrate importers.
+ *
+ * @group feeds_migrate
+ */
+class ImporterFormTest extends FeedsMigrateJavascriptTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'feeds_migrate',
+    'feeds_migrate_ui',
+    'feeds_migrate_test',
+    'node',
+    'user',
+  ];
+
+  /**
+   * Tests adding a new importer.
+   */
+  public function testAddImporter() {
+
+  }
+
+  /**
+   * Tests editing an existing importer.
+   */
+  public function testEditImporter() {
+
+  }
+
+  /**
+   * Tests deleting an importer.
+   */
+  public function testDeleteImporter() {
+
+  }
+
+}
