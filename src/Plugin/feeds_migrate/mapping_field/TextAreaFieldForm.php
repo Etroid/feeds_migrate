fa620f0d0680ec9c987cd91ea70bb3a450427a22
Martin Keereman <mkeereman@tableau.com>
2019-09-26 20:31:08 -0700

    Stub out new text area field mapping form

---
+++ src/Plugin/feeds_migrate/mapping_field/TextAreaFieldForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\feeds_migrate\Plugin\feeds_migrate\mapping_field;
+
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\feeds_migrate\MappingFieldFormBase;
+
+/**
+ * Class Text Area Mapping Field Form.
+ *
+ * @MappingFieldForm(
+ *   id = "text_area",
+ *   title = @Translation("Field mapping for text areas"),
+ *   fields = {
+ *     "text_long",
+ *     "text_with_summary",
+ *   }
+ * )
+ */
+class TextAreaFieldForm extends MappingFieldFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function buildProcessPluginsConfigurationForm(array $form, FormStateInterface $form_state, $property = NULL) {
+    if ($property === 'format') {
+      $test = 1;
+    }
+    else {
+      parent::buildProcessPluginsConfigurationForm($form, $form_state);
+    }
+  }
+
+}
