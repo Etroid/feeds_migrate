5b88aa297258d94caa494c067233c1ebda61dfbe
Martin Keereman <mkeereman@tableau.com>
2019-09-26 20:30:39 -0700

    Remove deprecated code

--- src/Plugin/feeds_migrate/mapping_field/DefaultFieldForm.php
+++ src/Plugin/feeds_migrate/mapping_field/DefaultFieldForm.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\feeds_migrate\Plugin\feeds_migrate\mapping_field;
 
-use Drupal\Core\Field\FieldConfigInterface;
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\feeds_migrate\MappingFieldFormBase;
 
 /**
@@ -11,35 +9,10 @@ use Drupal\feeds_migrate\MappingFieldFormBase;
  *
  * @MappingFieldForm(
  *   id = "default",
- *   title = @Translation("Default Field Processor"),
+ *   title = @Translation("Default Field Mapping"),
  *   fields = {}
  * )
  */
 class DefaultFieldForm extends MappingFieldFormBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getConfigurationFormMapping(array &$form, FormStateInterface $form_state) {
-    $mapping = [];
-    $properties = $form_state->getValue('properties');
-
-    if (!$properties) {
-      $mapping = parent::getConfigurationFormMapping($form, $form_state);
-    }
-    else {
-      // Handle nested field properties.
-      $properties = $form_state->getValue('properties');
-      foreach ($properties as $property => $info) {
-        $mapping['#properties'][$property] = [
-          'plugin' => 'get',
-          'source' => $info['source'],
-          '#process' => [], // todo get process lines from each plugin (i.e. tamper)
-        ];
-      }
-    }
-
-    return $mapping;
-  }
 
 }
